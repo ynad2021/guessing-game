b2d269a829dbdacb38972641ea43cea74bcb47b9
Yuliya.N <ynad2021@gmail.com>
2021-09-03 13:19:21 +0300

    feat: add solution to task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+    this.min = min;
+    this.max = max;
     }
 
     guess() {
-
+    return Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+    this.max = Math.round((this.max + this.min) / 2);
+    return this.max;
     }
 
     greater() {
-
+    this.min = Math.round((this.max + this.min) / 2);
+    return this.min;
     }
 }
 
